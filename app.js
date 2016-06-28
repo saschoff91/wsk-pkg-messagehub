154defe8264e32c7f57a17934ab77ea448aa8f35
saschoff91 <saschoff@de.ibm.com>
2016-06-28 18:48:15 +0200

    Insert module retry
    For correct deletion module retry is integrated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var http = require('http');
+//var http = require('http');
 
 var bodyParser = require('body-parser');
 
@@ -9,7 +9,16 @@ var request = require('request');
 
 var logger = require('./Logger');
 
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
+
+//retry routine while deleting kafka customer
+var retry = require('retry');
+var operation = retry.operation({
+	  retries: 4,           // try 1 time and retry 2 times if needed, total = 3
+	  minTimeout: 1 * 2000, // the number of milliseconds before starting the first retry
+	  maxTimeout: 3 * 2000  // the maximum number of milliseconds between two retries
+	});
+
 
 var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
@@ -19,6 +28,7 @@ var messageHub = appEnv.getServiceCreds('Message Hub-db');
  * Get message hub credentials
  */
 var messagehubApiKey = messageHub.api_key;
+
 var messagehubRestUrl =messageHub.kafka_rest_url;
 
 var routerHost = process.env.ROUTER_HOST || 'openwhisk.ng.bluemix.net';
@@ -28,30 +38,29 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.set('port', process.env.PORT || 80);
 
-messageTriggers={}; //data structure for all triggers
-requestLoops= {}; // data structure for all polling loops of each trigger
+feeds={}; //data structure for all triggers
+feedLoops= {}; // data structure for all polling loops of each trigger
 
 /*
  * Returns a list of all triggers (included inactive polling)
  */
 
-app.get('/messagehubtriggers', function(req, res) {
-	var method = 'GET / messagehubtrigger'
+app.get('/messagehubfeeds', function(req, res) {
+	var method = 'GET / messagehubfeeds'
 		var retn = {};
-	Object.keys(messageTriggers).forEach(function(id) {
+	Object.keys(feeds).forEach(function(id) {
 		logger.debug("OK", method, id);
-		var trigger = messageTriggers[id];
-		var triggerCopy = {};
-		Object.keys(trigger).forEach(function(key) {
-			if (key != 'feed')
-				triggerCopy[key] = trigger[key];
+		var feed = feeds[id];
+		var feedCopy = {};
+		Object.keys(feed).forEach(function(key) {
+			if (key != 'trigger')
+				feedCopy[key] = feed[key];
 		});
-		retn[id] = triggerCopy;
+		retn[id] = feedCopy;
 	});;
-	logger.info('output', method,'All triggers : ', retn);
+	logger.info('output', method,'All feeds : ', retn);
 	res.send(retn);
 	res.end();
 });
@@ -60,12 +69,12 @@ app.get('/messagehubtriggers', function(req, res) {
  * Create Trigger and start polling on a topic.
  * Create datastrucutre for trigger instance.
  */
-app.post('/messagehubtriggers',isAuthenticated, function(req, res) {
-	var method = 'POST / createMessageTrigger';
+app.post('/messagehubfeeds',isAuthenticated, function(req, res) {
+	var method = 'POST / createFeed';
 	var args = typeof req.body === 'object' ? req.body : JSON.parse(req.body);
 
 	if (!args.trigger) {
-		return sendError(method, 400, "Missing parameters: required id as triggername", res);
+		return sendError(method, 400, "Missing parameters: required id as feed name", res);
 	}
 	if (!args.topic) {
 		return sendError(method, 400, "Missing parameters: required topic for message hub listening", res);
@@ -73,35 +82,31 @@ app.post('/messagehubtriggers',isAuthenticated, function(req, res) {
 	if (!args.namespace) {
 		return sendError(method, 400, "Missing parameters: required namespace", res);
 	}
-	/*
-	if (!args.pollingInterval) {
-		return sendError(method, 400, "Missing parameters: required polling interval", res);
-	}*/
 
-	addMessageTrigger(args.trigger, args.topic, args.namespace, args.pollingInterval, function(messageTrigger){
-		if (messageTrigger.statusCode == 409) {
+	addFeed(args.trigger, args.topic, args.namespace, args.pollingInterval, function(response){
+		if (response.statusCode == 409) {
 			return sendError(method, 409, 'Error code 40902 – Consumer instance with the specified name already exists.', res);
 		}
-		if (messageTrigger.statusCode == 422) {
+		if (response.statusCode == 422) {
 			return sendError(method, 422, 'Error code 42204 – Invalid consumer configuration. One of the settings specified in the request contained an invalid value.', res);
 		} else {
-			return sendResponse(method, 200, "Trigger '"+args.trigger+"' created", res);
+			return sendResponse(method, 200, "Trigger and feed '"+args.trigger+"' created", res);
 		}
 	});	
 });
 
 /*
- * Associated function to POST/messagehubtriggers/:id
+ * Associated function to POST/messagehubfeeds/:id
  * Send POST request to message hub instance, to create a consumer (trigger)
  * Call method startPolling
  */
-function addMessageTrigger(id, topic, namespace, pollingInterval, _callback) {
-	var method = 'FUNCTION: addMessageTrigger';
-	
+function addFeed(id, topic, namespace, pollingInterval, _callback) {
+	var method = 'FUNCTION: addFeed';
+
 	if (!pollingInterval) {
 		pollingInterval = 5000; //default: 5s, if not specified
 	}
-	
+
 	var headers = {
 			'Content-Type': 'application/vnd.kafka.v1+json',
 			'X-Auth-Token': messagehubApiKey 
@@ -117,13 +122,44 @@ function addMessageTrigger(id, topic, namespace, pollingInterval, _callback) {
 
 	request(options, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
-			logger.info("OK", method, 'New Trigger ', id,' created on Topic "',topic , '" !');	
+			logger.info("OK", method, 'New Feed ', id,' created on Topic "',topic , '" !');	
 
-			messageTriggers[id] = {topic:topic, namespace:namespace, pollingInterval:pollingInterval, polling:false};
+			feeds[id] = {topic:topic, namespace:namespace, pollingInterval:pollingInterval, polling:false};
 
 			startPolling(id, topic, pollingInterval);
 		}
+		else {
+			deleteTriggerFeed(id);
+		}
 		_callback(response);
+	});
+}
+
+function deleteTriggerFeed(triggerName) {
+	var method = 'FUNCTION: deleteTrigger';
+	logger.info(triggerName, method, 'Cannot create trigger, cause of error in creation of feed');
+	var apiKey = "52a41dd3-ecc0-4eb2-af96-46af7083fa1c:eqd3fBvDmFpUZbU5WmuKoKucMAFEpMEkV21byvRYH7GIcCoAS45AJGMu2XLB5mUF";
+	//var form = {payload:message};
+	var auth = apiKey.split(':');
+	var uri = ' https://openwhisk.ng.bluemix.net/api/v1/namespaces/'+feeds[id].namespace+'/triggers/'+triggerName;
+
+	logger.info(tid, method, uri, message);
+	request({
+		method: 'DELETE',
+		uri: uri,
+		auth: {
+			user: auth[0],
+			pass: auth[1]
+		},
+		//json: form
+	}, function(error, response, body) {
+		logger.info(tid, method, 'done http request, STATUS', response ? response.statusCode : response);
+		logger.info(tid, method, 'done http request, body', body);
+		if (!error && response.statusCode == 200) {
+			logger.info(tid, method, body);
+		} else {
+			logger.error(tid, method, 'Error delete whisk trigger: ', response ? response.statusCode : response, error, body);
+		}
 	});
 }
 
@@ -131,7 +167,7 @@ function addMessageTrigger(id, topic, namespace, pollingInterval, _callback) {
  * Receive message for starting polling 
  * Call startPolling method
  */
-app.post('/messagehubtriggers/:id/startPolling/:topic',isAuthenticated,function(req, res) {
+app.post('/messagehubfeeds/:id/startPolling/:topic',isAuthenticated,function(req, res) {
 	var method = 'GET / startPolling';
 
 	var args = typeof req.body === 'object' ? req.body : JSON.parse(req.body);
@@ -143,8 +179,8 @@ app.post('/messagehubtriggers/:id/startPolling/:topic',isAuthenticated,function(
 
 	var pollingInterval = args.pollingInterval;
 
-	if ((messageTriggers[id])&&(messageTriggers[id].topic == topic)) {
-		if (messageTriggers[id].polling == false) {
+	if ((feeds[id])&&(feeds[id].topic == topic)) {
+		if (feeds[id].polling == false) {
 			startPolling(id, topic, pollingInterval);
 			return sendResponse(method, 200, "Trigger '"+id+"' start polling on topic '"+topic+"'", res);
 		} else {
@@ -178,35 +214,36 @@ function startPolling(id, topic, pollingInterval) {
 					logger.info(id, method, 'Incoming Message on topic ', topic,' with content ', response.body);
 					invokeWhiskAction(id, response.body);
 				}
+				/*
 				else { // for debbuging
 					logger.info(id, method, 'No new messages on ', topic);
-				}
+				}*/
 			} 
 		});
 	},pollingInterval);
-	requestLoops[id] = {feed:feed};
-	logger.info(id, method, 'Start Polling ',id,' on topic', topic, ' each ',pollingInterval, ' ms');
-	messageTriggers[id].polling = true;
-	messageTriggers[id].pollingInterval = pollingInterval;
-	return requestLoops[id];
+	feedLoops[id] = {feed:feed};
+	logger.info(id, method, 'Feed start polling ',id,' on topic', topic, ' each ',pollingInterval, ' ms');
+	feeds[id].polling = true;
+	feeds[id].pollingInterval = pollingInterval;
+	return feedLoops[id];
 }
 
 /*
  * Receive stop polling message for a trigger
  * Call stopPolling method
  */
-app.get('/messagehubtriggers/:id'+'/stopPolling',isAuthenticated, function(req, res) {
+app.get('/messagehubfeeds/:id'+'/stopPolling',isAuthenticated, function(req, res) {
 	var method = 'GET / stopPolling';
 
 	var args = typeof req.body === 'object' ? req.body : JSON.parse(req.body);
 	var id = req.params.id;
 	id = id.replace(/:/g, "/");	
 
-	if (messageTriggers[id].polling == true) {
+	if (feeds[id].polling == true) {
 		stopPolling(id);
-		return sendResponse(method, 200, "Trigger '"+id+"' stopped Polling", res);
+		return sendResponse(method, 200, "Feed '"+id+"' stopped Polling", res);
 	} else {
-		return sendError(method, 400, "Trigger '"+id+"' already stopped polling", res);
+		return sendError(method, 400, "Feed '"+id+"' already stopped polling", res);
 	}
 });
 
@@ -216,11 +253,11 @@ app.get('/messagehubtriggers/:id'+'/stopPolling',isAuthenticated, function(req, 
  */
 function stopPolling(id) {
 	var method = 'FUNCTION: stopPolling';
-	clearInterval(requestLoops[id].feed); //clear polling loop, stopping
-	delete requestLoops[id]; //remove from data structure 
-	logger.info(id, method, 'Stopping polling trigger ', id, ' ! ');
-	messageTriggers[id].polling = false;
-	messageTriggers[id].pollingInterval = 0;
+	clearInterval(feedLoops[id].feed); //clear polling loop, stopping
+	delete feedLoops[id]; //remove from data structure 
+	logger.info(id, method, 'Stopping polling feed ', id, ' ! ');
+	feeds[id].polling = false;
+	feeds[id].pollingInterval = 0;
 }
 
 
@@ -228,41 +265,60 @@ function stopPolling(id) {
  * Receive DELETE request for delete a trigger
  * First, stop polling, then call method for deletion of a trigger
  */
-app.delete('/messagehubtriggers/:id', isAuthenticated,function(req,res) {
-	var method = 'DELETE / messageTrigger';
+app.delete('/messagehubfeeds/:id', isAuthenticated,function(req,res) {
+	var method = 'DELETE / messageFeed';
 	var args = typeof req.body === 'object' ? req.body : JSON.parse(req.body);
 	var id = req.params.id;
 	id = id.replace(/:/g, "/");
 
-	if (messageTriggers[id]) {
-		if (messageTriggers[id].polling == true) {
+	if (feeds[id]) {
+		
+		if (feeds[id].polling == true) {
 			stopPolling(id);
 		}
-
-		deleteMessageTrigger(id, function(response){
-			if (response.statusCode == 204) {
-				return sendResponse(method, 204, 'Trigger "'+id+'" deleted', res);
+		
+		deleteRoutine(id, function(err,result) {
+			if (err) {
+				logger.info(id, method, 'Deleting trigger and feed ', id, ' failed ');
+				return sendError(method, 404, 'Feed "'+id+'" deletion failed.',res);
 			} else {
-				deleteMessageTrigger(id, function(response){
-					if (response.statusCode == 204) {
-						return sendResponse(method, 204, 'Trigger "'+id+'" deleted', res);
-					} else {
-						return sendError(method,409,'Error code 40910 - Another request is in progress for consumer "'+id+'". Request may be retried when response is received for the previous request.',res);
-					}
-				});
+				logger.info(id, method, 'Deleting trigger and feed ', id, ' done ');
+				return sendResponse(method, 200, 'Feed "'+id+'" deleted correctly.',res);
 			}
 		});
+		//deleteFeed(id, function(response){
+		//	if (response.statusCode == 204) {
+		//		return sendResponse(method, 204, 'Feed "'+id+'" deleted.');
+		//	} 
+		//});
 	} else {
 		return sendError(method, 404, "Error code 40403 – Consumer instance not found", res);
 	}
 });
 
+function deleteRoutine(input, callback) {
+	  operation.attempt(function(currentAttempt) {
+
+		    deleteFeed(input, function(err, result) {
+
+		      console.log('Current attempt: ' + currentAttempt);
+
+		      if (operation.retry(err)) {  // retry if needed
+		          return;
+		      }
+
+		      callback(err ? operation.mainError() : null, result);
+		    });
+		  });
+}
+
 /*
  * Send DELETE request to message hub to delete consumer (trigger)
  */
-function deleteMessageTrigger(id,_callback) {
-	var method = "FUNCTION: deleteTrigger"
-		var headers = {
+function deleteFeed(id,_callback) {
+
+	var method = "FUNCTION: deleteFeed";
+	var headers = {
 			'X-Auth-Token': messagehubApiKey 
 	};
 	var options = {
@@ -270,19 +326,16 @@ function deleteMessageTrigger(id,_callback) {
 			method: 'DELETE',
 			headers: headers
 	};
-	request(options, function(error, response, body) {
+	var req = request(options, function(error, response, body) {
 		if (response.statusCode == 204) {
-			logger.info("No Content", method, 'Trigger deleted ', id);
-			delete messageTriggers[id];
-			//_callback(response);
+			logger.info("No Content", method, 'Feed deleted ', id);
+			delete feeds[id];
+			_callback(null, 'ok');
 		} else {
-			if (response.statusCode == 409) {
-				logger.info("Conflict",method, 'Error code 40910 - Another request is in progress for consumer "'+id+'". Request may be retried when response is received for the previous request.')
-				//_callback(response);
-			}
+			logger.info("Conflict",method, 'Error code 40910 - Another request is in progress for consumer "'+id+'". Request may be retried when response is received for the previous request.')
+			_callback(new Error());
 		}
 
-		_callback(response);
 	});
 }
 
@@ -291,13 +344,13 @@ function deleteMessageTrigger(id,_callback) {
  * Fire the whisk trigger
  */
 function invokeWhiskAction(id, message) {
-	var method = 'invokeWhiskAction';
+	var method = 'FUNCTION: invokeWhiskAction';
 	var tid = "???";
-	logger.info(tid, method, 'for trigger', id, 'invoking action', id, 'with incoming message', JSON.stringify('payload:'+message));
+	logger.info(tid, method, 'for trigger', id, 'invoking action', id, 'with incoming message', message);
 	var apiKey = "52a41dd3-ecc0-4eb2-af96-46af7083fa1c:eqd3fBvDmFpUZbU5WmuKoKucMAFEpMEkV21byvRYH7GIcCoAS45AJGMu2XLB5mUF";
 	var form = {payload:message};
 	var auth = apiKey.split(':');
-	var uri = ' https://openwhisk.ng.bluemix.net/api/v1/namespaces/'+messageTriggers[id].namespace+'/triggers/'+id;
+	var uri = ' https://openwhisk.ng.bluemix.net/api/v1/namespaces/'+feeds[id].namespace+'/triggers/'+id;
 
 	logger.info(tid, method, uri, message);
 	request({
